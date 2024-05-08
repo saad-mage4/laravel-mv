099ece252da2068850f707277952863ed9218e09
saad-mage4 <saad@mage4.com>
2024-05-09 00:12:36 +0500

    highlight-page css

--- resources/views/seller/product_highlight.blade.php
+++ resources/views/seller/product_highlight.blade.php
@@ -3,6 +3,68 @@
     <title>{{__('user.Product Highlight')}}</title>
 @endsection
 @section('seller-content')
+
+<style>
+    #card-element {
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        padding: 10px;
+        max-width:450px;
+    }
+    
+    .radio-groups label {
+        color: #fff;
+        font-weight: bold;
+        letter-spacing: 1px;
+        margin-right: 10px;
+    }
+
+    button[type="submit"] {
+        background-color: #007bff;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button[type="submit"]:hover {
+        background-color: #0056b3;
+    }
+    .highlight-text {
+        display: flex;
+        flex-direction: column;
+    }
+    .radio-groups {
+        display: flex;
+        gap:20px;
+        margin-block:15px;
+    }
+    .radio-group * {
+        margin: 0;
+        padding: 0;
+    }
+    .radio-group {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        background-color: #2046da;
+        border:1px solid #2046da;
+        padding:5px;
+        border-radius:5px;
+    }
+    .highlight-text {
+        color: #444;
+    }
+    .highlight-text .highlight {
+        font-weight: bold;
+        color: #000;
+    }
+    .highlight-text span:not(.highlight) {
+        margin-left: 10px;
+        color: #222;
+    }
+</style>
     <!-- Main Content -->
     <div class="main-content">
         <section class="section">
@@ -14,7 +76,7 @@
                     <div class="breadcrumb-item">{{__('user.Product Highlight')}}</div>
                 </div>
             </div>
-
+ 
             <div class="section-body">
                 <a href="{{ route('seller.product.index') }}" class="btn btn-primary"><i
                         class="fas fa-list"></i> {{__('user.Products')}}</a>
@@ -22,6 +84,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
+                                @if($product->is_highlight_1 != 0)
                                 <form action="{{ route('seller.update-product-highlight',$product->id) }}"
                                       method="POST">
                                     @method('PUT')
@@ -59,32 +122,39 @@
                                             </div>
                                         @endif
                                     </div>
-{{--                                    <div class="row">--}}
-{{--                                        <div class="col-12">--}}
-{{--                                            <button class="btn btn-primary">{{__('user.Update')}}</button>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-                                    <div class="row" style="{{ $product->is_highlight_1 == 0 ? 'display: none;' : '' }}">
+                                    <div class="row">
                                         <div class="col-12">
                                             <button class="btn btn-primary">{{__('user.Update')}}</button>
                                         </div>
                                     </div>
                                 </form>
+                                @endif
                                 <form id="payment-form" method="POST" action="{{ route('seller.product.highlight.payment', ['id' => $product->id]) }}" style="{{ $product->is_highlight_1 == 0 ? 'display: block;' : 'display: none;' }}">
                                     @csrf
+                                    <div class="highlight-text">
+                                        <span class="highlight">Upgrade your Product Highlight:</span>
+                                        <span>1-week highlight: €5</span>
+                                        <span>2-week highlight: €10</span>
+                                        Please proceed with payment to activate your chosen option.
+                                    </div>
+                                    <div class="radio-groups">
+                                        <div class="radio-group">
+                                            <input type="radio" checked id="5-euro" name="amount" value="5">
+                                            <label for="5-euro">5 Euros</label>
+                                        </div>
+                                        <div class="radio-group">
+                                            <input type="radio" id="10-euro" name="amount" value="10">
+                                            <label for="10-euro">10 Euros</label>
+                                        </div>
+                                    </div>
                                     <!-- Include Stripe Elements for collecting card details -->
                                     <div id="card-element"><!-- Stripe Elements will be inserted here --></div>
 
                                     <!-- Include input fields for additional payment information (e.g., email) -->
-                                    <input type="email" name="email" placeholder="Email">
 
                                     <!-- Include a hidden input for the generated token -->
                                     <input type="hidden" name="stripeToken" id="stripeToken" value="">
 
-                                    <input type="radio" id="amount" name="amount" value="5">
-                                    <label for="html">5 Euros</label><br>
-                                    <input type="radio" id="amount" name="amount" value="10">
-                                    <label for="css">10 Euros</label><br>
 
                                     <button type="submit">Submit Payment</button>
                                 </form>
