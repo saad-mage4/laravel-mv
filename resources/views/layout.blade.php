507a693a20499669dd3f14c1b684c6d64189e951
jahazaib321 <jahazaib@mage4.com>
2024-05-21 21:40:25 +0500

    new changes 3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -281,7 +281,7 @@
                 <h5>{{__('user.Sub Total')}} <span>{{ $currencySetting->currency_icon }}{{ $sidebarCartSubTotal }}</span></h5>
                 <div class="wsus__minicart_btn_area">
                     <a class="common_btn" href="{{ route('cart') }}">{{__('user.View Cart')}}</a>
-                    <a class="common_btn" href="{{ route('user.checkout.billing-address') }}">{{__('user.Checkout')}}</a>
+                    <a class="common_btn" href="{{ route('user.checkout.checkout') }}">{{__('user.Checkout')}}</a>
                 </div>
             @endif
             </div>
