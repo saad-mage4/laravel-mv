27cfb66126831e18e46dcf1a40bfd97b959a737d
daniyal <daniyal@mage4.com>
2024-06-04 20:07:41 +0500

    add and update sponsors

--- app/Http/Controllers/Seller/SellerSponsorController.php
+++ app/Http/Controllers/Seller/SellerSponsorController.php
@@ -3,19 +3,39 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sponsorships;
+use Illuminate\Contracts\{Foundation\Application, View\Factory, View\View};
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, DB};
 
 class SellerSponsorController extends Controller
 {
     public function index() {
 
     }
-    public function showSponsor(Request $request) {
+
+    /**
+     * @return Application|Factory|View
+     */
+    public function showSponsor() {
         $user = Auth::guard('web')->user();
-        return view('seller.show_sponsor');
+        $banners = DB::table('sponsorships')->get();
+        return view('seller.show_sponsor', compact('banners'));
     }
-    public function addSponsorReq(Request $request) {
-        dd($request);
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function addSponsorReq(Request $request): string
+    {
+        if (DB::table('sponsorships')->where('banner_name', $request->banner_name)->exists()) {
+            Sponsorships::class->updateSponsor($request);
+            $response = 'Banner has been updated successfully!';
+        } else {
+            Sponsorships::class->updateSponsor($request);
+            $response = 'Banner has been added successfully!';
+        }
+        return $response;
     }
 }
--- app/Models/Sponsorships.php
+++ app/Models/Sponsorships.php
@@ -2,10 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Factories\HasFactory, Model};
+use Illuminate\Support\Facades\DB;
 
 class Sponsorships extends Model
 {
     use HasFactory;
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function addSponsor($params)
+    {
+        DB::table('sponsorships')->insert(
+            [
+                'banner_name' => $params->banner_name,
+                'width' => $params->width,
+                'height' => $params->height,
+                'price' => $params->price,
+                'hours' => $params->hours,
+                'is_booked' => $params->is_booked,
+                'image_url' => $params->image_url,
+                'sponsor_title' => $params->sponsor_title,
+                'sponsor_name' => $params->sponsor_name,
+            ]
+        );
+    }
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function updateSponsor($params)
+    {
+        DB::table('sponsorships')->where('banner_name', $params->banner_name)->update(
+            [
+                ['width' => $params->width],
+                ['height' => $params->height],
+                ['price' => $params->price],
+                ['hours' => $params->hours],
+                ['is_booked' => $params->is_booked],
+                ['image_url' => $params->image_url],
+                ['sponsor_title' => $params->sponsor_title],
+                ['sponsor_name' => $params->sponsor_name],
+            ]
+        );
+    }
 }
