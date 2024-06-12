fe92d7f3d40b3e154132655103568204632e6643
saad-mage4 <saad@mage4.com>
2024-06-12 19:08:32 +0500

    change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('App\Http\Controllers\Seller\WithdrawController@automateWithdraw')
-            ->everyMinute()->timezone('Asia/Karachi');
+        // $schedule->call('App\Http\Controllers\Seller\WithdrawController@automateWithdraw')
+        //     ->everyMinute()->timezone('Asia/Karachi');
 
         $schedule->call('App\Http\Controllers\Seller\SellerSponsorController@bannerRemoveCron')
             ->everyMinute()->timezone('Asia/Karachi');
