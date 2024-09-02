612268f7b4e7f0f387c673891f06559ea145f5d7
saad-mage4 <saad@mage4.com>
2024-09-02 19:33:39 +0500

    client tawk.to added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1542,18 +1542,18 @@
     </script>
 
 <!--Start of Tawk.to Script-->
-{{-- <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/65e09e099131ed19d973453e/1hnqo3i5k';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script> --}}
-<!--End of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/66cf7ff0ea492f34bc0b21f6/1i6d8ngvk';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
 </body>
 
