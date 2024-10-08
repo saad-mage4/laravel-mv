0f06f61bfda26b650a87529d1b74f85efbecad40
Taha Ahmed <taha@mage4.com>
2024-10-08 18:36:37 +0500

    CheckSubdomain.php

--- app/Http/Middleware/CheckSubdomain.php
+++ app/Http/Middleware/CheckSubdomain.php
@@ -18,14 +18,28 @@ class CheckSubdomain
     public function handle(Request $request, Closure $next)
     {
         // Get the current subdomain
-        $subdomain = explode('.', $request->getHost())[0];
+        // $subdomain = explode('.', $request->getHost())[0];
 
-        // Check if it's the membri subdomain
-        if ($subdomain === env('SELLER_APP_URL')) {
-            // Allow access only to seller.dashboard
-            if (!Route::currentRouteNamed('seller.dashboard')) {
-                abort(403, 'Access denied');
-            }
+        // if ($subdomain === env('SELLER_APP_URL')) {
+        //     if (!Route::currentRouteNamed('seller.dashboard')) {
+        //         abort(403, 'Access denied');
+        //     }
+        // }
+
+        // return $next($request);
+
+
+
+        // Get the current subdomain
+        $hostParts = explode('.', $request->getHost());
+
+        // Example: 'membri.mage4dev.com' -> subdomain = 'membri'
+        $subdomain = count($hostParts) > 2 ? $hostParts[0] : null;
+
+        // Ensure it's the seller's subdomain
+        if ($subdomain !== env('SELLER_APP_URL')) {
+            // Restrict access for non-seller subdomains
+            return abort(403, 'Access denied');
         }
 
         return $next($request);
